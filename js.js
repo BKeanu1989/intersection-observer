5396e970d28c3cf3e0468321df0f455c546268b8
Kevin Fechner <bkevinfechner1989@gmail.com>
2017-09-30 02:14:51 +0200

    finally working ... for something so simpel x)

--- js.js
+++ js.js
@@ -1,53 +1,26 @@
-console.log("xx");
-
 function exampleCallback(entries, observer) {
-	// console.log("reached", entries);
-
-	// console.log("target", entries[0].target);
-	// console.log("target data-src", entries[0].target.dataset.src);
-	// console.log("target src", entries[0].target.src);
-
-	// console.log("observer", observer);
-	console.log("observed!");
-
+	entries.forEach(function(entry) {
+		if (entry.isIntersecting && entry.target.dataset.loaded != "true") {
+			entry.target.src = entry.target.dataset.src;
+			entry.target.dataset.loaded = true;
+		}
+	});
 }
+startUp();
 
-// var observer = [];
-var observer;
+function startUp() {
 
-var options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 1
-};
+	var observer = [];
+	var options = {
+	  root: null,
+	  rootMargin: "0px",
+	  threshold: 0
+	};
 
-var firstImage = document.querySelector('img.first');
-var secondImage = document.querySelector('img.second');
-// var thirdImage = document.querySelector('img.third');
-// var fourthImage = document.querySelector('img.fourth');
-// var fifthImage = document.querySelector('img.fifth');
-console.log(secondImage);
-var images = document.querySelectorAll('img');
+	var images = document.querySelectorAll('img');
 
-// for (let i = 0; i < images.length; i++) {
-// 	observer[i]= new IntersectionObserver(exampleCallback, options);
-// 	console.log(images[i]);
-// 	observer[i].observe(images[i]);
-// }
-
-console.log("observer array: ",observer);
-
-// observer = new IntersectionObserver(exampleCallback, options);
-// observer.observe(images);
-
-observer = new IntersectionObserver(exampleCallback, options);
-observer.observe(secondImage);
-observer.observe(firstImage);
-// for (let i = 0; i < images.length; i++) {
-// 	observer.observe(images[i]);
-// }
-
-// images.forEach(function(x,IntersectionObserverdex) {
-// 	observer[index] = new IntersectionObserver(exampleCallback, options);
-// 	observer[index].observe(x);
-// });
+	for (let i = 0; i < images.length; i++) {
+		observer[i]= new IntersectionObserver(exampleCallback, options);
+		observer[i].observe(images[i]);
+	}
+}
