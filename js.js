1946b5c8fe85a7ae9a63616afd47a3b063d90591
Kevin Fechner <bkevinfechner1989@gmail.com>
2017-10-09 02:02:18 +0200

    more images; add safari 'tests';

--- js.js
+++ js.js
@@ -1,6 +1,8 @@
 function exampleCallback(entries, observer) {
 	entries.forEach(function(entry) {
 		if (entry.isIntersecting && entry.target.dataset.loaded != "true") {
+			// debug for safari
+			console.log(entry);
 			entry.target.src = entry.target.dataset.src;
 			entry.target.dataset.loaded = true;
 		}
@@ -9,8 +11,6 @@ function exampleCallback(entries, observer) {
 startUp();
 
 function startUp() {
-
-	var observer = [];
 	var options = {
 	  root: null,
 	  rootMargin: "0px",
@@ -19,8 +19,18 @@ function startUp() {
 
 	var images = document.querySelectorAll('img');
 
-	for (let i = 0; i < images.length; i++) {
+	// seems to be necessary for safari
+	// loads all resources if at the top of the site
+	var observer = [];
+	for (var i = 0; i < images.length; i++) {
+		// console.log(images[i]);
+		// options.root = images[i];
 		observer[i]= new IntersectionObserver(exampleCallback, options);
 		observer[i].observe(images[i]);
 	}
+	// var observer;
+	// observer = new IntersectionObserver(exampleCallback, options);
+	// for (var i = 0; i < images.length; i++) {
+	// 	observer.observe(images[i]);
+	// }
 }
